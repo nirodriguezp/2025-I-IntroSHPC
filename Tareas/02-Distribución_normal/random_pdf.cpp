d3ba4d796c5981f8d06504c7e2d3bbdf08d621f6
NicolasP <nirodriguezp@unal.edu.co>
2025-04-30 13:09:11 -0500

    Cálculo de la PDF teórica agregado

--- Tareas/02-Distribución_normal/random_pdf.cpp
+++ Tareas/02-Distribución_normal/random_pdf.cpp
@@ -2,8 +2,10 @@
 #include <iostream>
 #include <cstdlib>
 #include <vector>
+#include <cmath>
 
 void compute_pdf(int seed, int nsamples, double mu, double sigma, double xmin, double xmax, int nbins);
+double teorical_pdf(double x, double mu, double sigma);
 
 int main(int argc, char **argv)
 {
@@ -25,23 +27,31 @@ void compute_pdf(int seed, int nsamples, double mu, double sigma, double xmin, d
   std::normal_distribution<double> dis{mu, sigma};
 
   // Histogram stuff
-  std::vector<int> histogram(nbins);
-  double x_width = (xmax - xmin) / nbins;
+  std::vector<int> histogram(nbins); // Vector que almacena las frecuencias
+  double x_width = (xmax - xmin) / nbins; // El ancho de las barras del histograma
 
   for(int n = 0; n < nsamples; ++n) {
     double r = dis(gen);
 
-    // TODO: fill here the counting histogram stuff
+    // Fill here the counting histogram stuff
     for(int i = 0; i < nbins; i++)
     {
-        if (r >= xmin + i * x_width && r < xmin + (i+1) * x_width){
-            histogram[i] += 1;
+        if (r >= xmin + i * x_width && r < xmin + (i+1) * x_width){ // Verifica en qué intervalo cae el número aleatorio
+            histogram[i] += 1;                                      // Si el número aleatorio caen en el intervalo i, suma 1
         }
     }
   }
-  // TODO: compute and print the pdf
+  // Compute and print the pdf
+  double x = 0.0, pdf = 0.0;
   for(int i = 0; i < nbins; i++) {
-
-    std::cout << xmin + x_width * (0.5 + i) << "\t" << histogram[i] << "\n";
+    x = xmin + x_width * (0.5 + i); // Centro de la barra
+    double pdf_teo = teorical_pdf(x, mu, sigma);
+    double pdf_est = histogram[i] / (nsamples * x_width);
+    std::cout << x << "\t" << pdf_est << "\t" << pdf_teo << "\n";
   }
+}
+
+double teorical_pdf(double x, double mu, double sigma)
+{
+  return (1.0 / (sigma * std::sqrt(2 * M_PI))) * std::exp(-0.5 * std::pow((x - mu) / sigma, 2));
 }
